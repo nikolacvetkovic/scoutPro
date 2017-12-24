f197412327cbf4ae41cf41992909a40c7f9d161d
Nikola Cvetkovic <nikola.cvetkovic91@gmail.com>
2017-12-24 23:25:48 +0100

    move annotation @EnableAsync to config class

--- src/main/java/com/riocode/scoutpro/ScoutProApp.java
+++ src/main/java/com/riocode/scoutpro/ScoutProApp.java
@@ -2,10 +2,8 @@ package com.riocode.scoutpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class ScoutProApp {
     public static void main( String[] args ){
         SpringApplication.run(ScoutProApp.class, args);
