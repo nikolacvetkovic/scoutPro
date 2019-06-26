c6bc7d4c9cf855c58b659c49ab2fc826c3d7f86c
cvelenidza <nikola.cvetkovic91@gmail.com>
2019-06-26 21:39:47 +0000

    implement AppUserService methods, add custom exceptions

--- src/main/java/xyz/riocode/scoutpro/service/AppUserServiceImpl.java
+++ src/main/java/xyz/riocode/scoutpro/service/AppUserServiceImpl.java
@@ -1,22 +1,42 @@
 package xyz.riocode.scoutpro.service;
 
 import org.springframework.stereotype.Component;
+import xyz.riocode.scoutpro.exception.AppUserNotFoundException;
+import xyz.riocode.scoutpro.exception.DuplicateAppUserUsernameException;
 import xyz.riocode.scoutpro.model.AppUser;
+import xyz.riocode.scoutpro.repository.AppUserRepository;
 
 @Component
 public class AppUserServiceImpl implements AppUserService {
+
+    private final AppUserRepository appUserRepository;
+
+    public AppUserServiceImpl(AppUserRepository appUserRepository) {
+        this.appUserRepository = appUserRepository;
+    }
+
     @Override
     public AppUser create(AppUser appUser) {
-        return null;
+        appUserRepository.findByUsername(appUser.getUsername())
+                .ifPresent(u -> {
+                    throw new DuplicateAppUserUsernameException();
+                });
+
+        return appUserRepository.save(appUser);
     }
 
     @Override
     public AppUser changePassword(AppUser appUser) {
-        return null;
+        AppUser user = appUserRepository.findByUsername(appUser.getUsername())
+            .orElseThrow(AppUserNotFoundException::new);
+
+        user.setPassword(appUser.getPassword());
+
+        return appUserRepository.save(user);
     }
 
     @Override
     public void deleteById(Long userId) {
-
+        appUserRepository.deleteById(userId);
     }
 }
--- src/test/java/xyz/riocode/scoutpro/service/AppUserServiceImplTest.java
+++ src/test/java/xyz/riocode/scoutpro/service/AppUserServiceImplTest.java
@@ -1,5 +1,103 @@
 package xyz.riocode.scoutpro.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.EmptyResultDataAccessException;
+import xyz.riocode.scoutpro.exception.AppUserNotFoundException;
+import xyz.riocode.scoutpro.exception.DuplicateAppUserUsernameException;
+import xyz.riocode.scoutpro.model.AppUser;
+import xyz.riocode.scoutpro.repository.AppUserRepository;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 class AppUserServiceImplTest {
+
+
+    @Mock
+    AppUserRepository appUserRepository;
+
+    @InjectMocks
+    AppUserServiceImpl appUserService;
+
+    AppUser user;
+
+    @BeforeEach
+    void setUp(){
+        user = new AppUser();
+        user.setId(1L);
+        user.setUsername("cvelenidza");
+        user.setPassword("123456");
+    }
+
+    @Test
+    void testDeleteByIdOk(){
+        appUserService.deleteById(1L);
+
+        verify(appUserRepository).deleteById(anyLong());
+    }
+
+    @Test
+    void testDeleteByIdNotFound(){
+        doThrow(EmptyResultDataAccessException.class).when(appUserRepository).deleteById(anyLong());
+
+        assertThrows(EmptyResultDataAccessException.class, () -> appUserService.deleteById(1L));
+
+        verify(appUserRepository).deleteById(anyLong());
+    }
+
+    @Test
+    void createUserOk(){
+        AppUser userToReturn = new AppUser();
+        userToReturn.setId(1L);
+        userToReturn.setUsername("cvelenidza");
+
+        when(appUserRepository.save(any())).thenReturn(userToReturn);
+
+        AppUser savedUser = appUserService.create(user);
+
+        assertEquals(user.getId(), savedUser.getId());
+        assertEquals(user.getUsername(), savedUser.getUsername());
+        verify(appUserRepository).findByUsername(anyString());
+    }
+
+    @Test
+    void createUserDuplicateUsername(){
+        when(appUserRepository.save(user)).thenThrow(DuplicateAppUserUsernameException.class);
+
+        assertThrows(DuplicateAppUserUsernameException.class, () -> appUserService.create(user));
+
+        verify(appUserRepository).findByUsername(anyString());
+    }
+
+    @Test
+    void changePasswordOk(){
+        AppUser userToReturn = new AppUser();
+        userToReturn.setId(1L);
+        userToReturn.setUsername("cvelenidza");
+        userToReturn.setPassword("123456");
+
+        when(appUserRepository.findByUsername(anyString())).thenReturn(Optional.of(new AppUser()));
+        when(appUserRepository.save(any())).thenReturn(userToReturn);
+
+        AppUser updatedUser = appUserService.changePassword(user);
+
+        assertEquals(userToReturn.getPassword(), updatedUser.getPassword());
+    }
+
+    @Test
+    void changePasswordUserNotExists(){
+        assertThrows(AppUserNotFoundException.class, () -> appUserService.changePassword(user));
+    }
 
 }
