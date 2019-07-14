579174172e23194d451a8cb79c4e53a55f823b41
cvelenidza <nikola.cvetkovic91@gmail.com>
2019-07-14 21:36:43 +0000

    implement and write tests

--- src/main/java/xyz/riocode/scoutpro/jpa/converter/SetStringStringConverter.java
+++ src/main/java/xyz/riocode/scoutpro/jpa/converter/SetStringStringConverter.java
@@ -2,18 +2,20 @@ package xyz.riocode.scoutpro.jpa.converter;
 
 import javax.persistence.AttributeConverter;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class SetStringStringConverter implements AttributeConverter<Set<String>, String> {
     @Override
     public String convertToDatabaseColumn(Set<String> strings) {
-        return strings.stream().collect(Collectors.joining());
+        if(strings.size() == 0) return null;
+        return String.join(",", strings);
     }
 
     @Override
     public Set<String> convertToEntityAttribute(String s) {
-        return new HashSet<>(Arrays.asList(s));
+        if (s == null) return Collections.EMPTY_SET;
+        return new HashSet<>(Arrays.asList(s.split(",")));
     }
 }
