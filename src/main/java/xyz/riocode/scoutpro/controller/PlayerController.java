fca4057b5599dea6831d4a5d674c92e69a1bccc5
cvelenidza <nikola.cvetkovic91@gmail.com>
2019-08-17 15:32:37 +0100

    arrange templates, delete controller, set up static and dynamic urls

--- src/main/java/xyz/riocode/scoutpro/controller/PlayerController.java
+++ src/main/java/xyz/riocode/scoutpro/controller/PlayerController.java
@@ -9,7 +9,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import xyz.riocode.scoutpro.converter.PlayerConverter;
 import xyz.riocode.scoutpro.dto.PlayerDashboardDTO;
 import xyz.riocode.scoutpro.dto.PlayerFormDTO;
@@ -20,7 +19,6 @@ import javax.validation.Valid;
 import java.util.Set;
 
 @Controller
-@RequestMapping("/player")
 public class PlayerController {
     
     private final PlayerService playerService;
@@ -31,59 +29,74 @@ public class PlayerController {
         this.playerConverter = playerConverter;
     }
 
-    @GetMapping("/new")
-    public String showPlayerForm(ModelMap modelMap){
-        modelMap.addAttribute("player", new PlayerFormDTO());
-        return "playerForm";
+    @GetMapping({"/", "/dashboard"})
+    public String showDashboard(){
+        return "player/dashboard";
     }
 
-    @PostMapping("/new")
+    @GetMapping("player/new")
+    public String showPlayerForm(ModelMap modelMap){
+        modelMap.addAttribute("player", new PlayerFormDTO());
+        return "player/playerForm";
+    }
+
+    @PostMapping("player/new")
     public String save(@Valid PlayerFormDTO player, BindingResult bindingResult){
         if(bindingResult.hasErrors()) return "playerForm";
         playerService.create(playerConverter.playerFormDTOToPlayer(player, "cvele"), "cvele");
         return "redirect:/dashboard";
     }
 
-    @GetMapping(value = "/{pageNumber}/page", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "player/{pageNumber}/page", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Set<PlayerDashboardDTO>> getPlayers(@PathVariable int pageNumber){
         return new ResponseEntity<>(playerConverter.playerToPlayerDashboardDTO(playerService.getByUserPaging("cvele", pageNumber), "cvele"), HttpStatus.OK);
     }
 
-    @GetMapping("/{playerId}/edit")
+    @GetMapping("player/{playerId}/edit")
     public String showPlayerFormForEdit(@PathVariable Long playerId, ModelMap modelMap){
         PlayerFormDTO foundPlayer = playerConverter.playerToPlayerFormDTO(playerService.getByIdAndUser(playerId, "cvele"), "cvele");
         modelMap.addAttribute("player", foundPlayer);
-        return "playerForm";
+        return "player/playerForm";
     }
 
-    @PostMapping("/edit")
+    @PostMapping("player/edit")
     public String update(@Valid PlayerFormDTO playerFormDTO, BindingResult bindingResult){
         if(bindingResult.hasErrors()) return "playerForm";
         playerService.update(playerConverter.playerFormDTOToPlayer(playerFormDTO, "cvele"), "cvele");
         return "redirect:/dashboard";
     }
 
-    @GetMapping(value = "/{playerId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "player/{playerId}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Player> getPlayerById(@PathVariable Long playerId, ModelMap modelMap){
         return new ResponseEntity<>(playerService.getByIdAndUser(playerId, "cvele"), HttpStatus.OK);
     }
 
-    @GetMapping("/{playerId}/show")
+    @GetMapping("player/{playerId}/show")
     public String show(@PathVariable Long playerId, ModelMap modelMap){
         return "";
     }
 
-    @GetMapping("/{playerName}/name")
+    @GetMapping("player/{playerName}/name")
     public String getPlayerByName(@PathVariable Long playerName, ModelMap modelMap){
         return "";
     }
 
-    @GetMapping("/{playerId}/compare")
+    @GetMapping("player/{playerId}/compare")
     public String compare(@PathVariable Long playerId, ModelMap modelMap){
-        return "";
+        return "player/compare";
     }
 
-    @GetMapping("/{playerId}/delete")
+    @GetMapping("player/compare")
+    public String compare(){
+        return "player/compare";
+    }
+
+    @GetMapping("player/transfersandnews")
+    public String showTransfersAndNews(){
+        return "player/transfersAndNews";
+    }
+
+    @GetMapping("player/{playerId}/delete")
     public String delete(@PathVariable Long playerId){
         return "";
     }
