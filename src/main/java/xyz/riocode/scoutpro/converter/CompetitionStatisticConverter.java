ae87d3540eaeaec9d0f38d4ac2cb0519cbabc0a6
Nikola <nikola.cvetkovic91@gmail.com>
2019-09-25 15:44:15 +0200

    implement sorting

--- src/main/java/xyz/riocode/scoutpro/converter/CompetitionStatisticConverter.java
+++ src/main/java/xyz/riocode/scoutpro/converter/CompetitionStatisticConverter.java
@@ -4,30 +4,37 @@ import org.springframework.stereotype.Component;
 import xyz.riocode.scoutpro.dto.CompetitionStatisticDTO;
 import xyz.riocode.scoutpro.model.CompetitionStatistic;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Component
 public class CompetitionStatisticConverter {
 
-    public List<CompetitionStatisticDTO> competitionStatisticsToCompetitionStatisticDTOs(Set<CompetitionStatistic> competitionStatistics){
-        return competitionStatistics.stream().map(competitionStatistic -> {
+    public List<CompetitionStatisticDTO> competitionStatisticsToCompetitionStatisticDTOs(Set<CompetitionStatistic> competitionStatistics) {
+        List<CompetitionStatisticDTO> competitionStatisticDTOS = new ArrayList<>();
+        CompetitionStatisticDTO last = null;
+        for (CompetitionStatistic cs : competitionStatistics) {
             CompetitionStatisticDTO competitionStatisticDTO = new CompetitionStatisticDTO();
-            competitionStatisticDTO.setCompetition(competitionStatistic.getCompetition());
-            competitionStatisticDTO.setStartedApps(competitionStatistic.getStartedApps());
-            competitionStatisticDTO.setSubApps(competitionStatistic.getSubApps());
-            competitionStatisticDTO.setGoals(competitionStatistic.getGoals());
-            competitionStatisticDTO.setAssists(competitionStatistic.getAssists());
-            competitionStatisticDTO.setYellowCards(competitionStatistic.getYellowCards());
-            competitionStatisticDTO.setRedCards(competitionStatistic.getRedCards());
-            competitionStatisticDTO.setShotsPerGame(competitionStatistic.getShotsPerGame().toString());
-            competitionStatisticDTO.setPassSuccess(competitionStatistic.getPassSuccess().toString());
-            competitionStatisticDTO.setAerialsWon(competitionStatistic.getAerialsWon().toString());
-            competitionStatisticDTO.setManOfTheMatch(competitionStatistic.getManOfTheMatch());
-            competitionStatisticDTO.setRating(competitionStatistic.getRating().toString());
-            return competitionStatisticDTO;
-        }).collect(Collectors.toList());
+            competitionStatisticDTO.setCompetition(cs.getCompetition());
+            competitionStatisticDTO.setStartedApps(cs.getStartedApps());
+            competitionStatisticDTO.setSubApps(cs.getSubApps());
+            competitionStatisticDTO.setGoals(cs.getGoals());
+            competitionStatisticDTO.setAssists(cs.getAssists());
+            competitionStatisticDTO.setYellowCards(cs.getYellowCards());
+            competitionStatisticDTO.setRedCards(cs.getRedCards());
+            competitionStatisticDTO.setShotsPerGame(cs.getShotsPerGame().toString());
+            competitionStatisticDTO.setPassSuccess(cs.getPassSuccess().toString());
+            competitionStatisticDTO.setAerialsWon(cs.getAerialsWon().toString());
+            competitionStatisticDTO.setManOfTheMatch(cs.getManOfTheMatch());
+            competitionStatisticDTO.setRating(cs.getRating().toString());
+            if (competitionStatisticDTO.getCompetition().equals("Total / Average")) {
+                last = competitionStatisticDTO;
+                continue;
+            }
+            competitionStatisticDTOS.add(competitionStatisticDTO);
+        }
+        competitionStatisticDTOS.add(last);
+        return competitionStatisticDTOS;
     }
-
 }
