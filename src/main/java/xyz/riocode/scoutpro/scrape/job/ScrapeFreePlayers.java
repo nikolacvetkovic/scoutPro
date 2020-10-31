e592c2eb20d8b18b87f7fac520b5b390ba99a215
Nikola <nikolac@telekom.rs>
2020-10-31 20:32:27 +0100

    update with data for new pes season

--- src/main/java/xyz/riocode/scoutpro/scrape/job/ScrapeFreePlayers.java
+++ src/main/java/xyz/riocode/scoutpro/scrape/job/ScrapeFreePlayers.java
@@ -6,6 +6,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -25,8 +26,8 @@ import java.util.stream.Collectors;
 //@Component
 public class ScrapeFreePlayers implements CommandLineRunner {
 
-    private static final String PESDB_SEARCH_BASE_URL = "http://pesdb.net/pes2020/?pos=1&page=";
-    private static final String PESDB_BASE_URL = "http://pesdb.net/pes2020";
+    private static final String PESDB_SEARCH_BASE_URL = "http://pesdb.net/pes2021/?pos=3&page=";
+    private static final String PESDB_BASE_URL = "http://pesdb.net/pes2021";
     private static final String PSML_SEARCH_BASE_URL = "http://psml.rs/index.php?action=aps&q=&pesdblink=";
     private static final String PSML_BASE_URL = "http://psml.rs/index.php";
     private static final String WS_SEARCH_BASE_URL = "https://www.whoscored.com/Search/?t=";
@@ -59,19 +60,19 @@ public class ScrapeFreePlayers implements CommandLineRunner {
                 //if (ScrapeHelper.getElement(psmlPlayer, "td:nth-of-type(8) a") != null){ log.warn("Player is in team already"); continue;}
                 String psmlQueryUrl = ScrapeHelper.getAttributeValue(psmlPlayer, "td:nth-of-type(1) a", "href");
                 String transfermarktUrl = ScrapeHelper.getAttributeValue(psmlPlayer, "td:nth-of-type(3) a", "href");
-                Document tmPlayerPage = Jsoup.connect(transfermarktUrl).get();
-                String playerName = ScrapeHelper.getElementData(tmPlayerPage, "h1[itemprop=name]");
-                Document wsSearchResult = Jsoup.connect(WS_SEARCH_BASE_URL + playerName.replaceAll(" ", "+")).get();
-                String wsQueryUrl = ScrapeHelper.getAttributeValue(wsSearchResult, "div.search-result tr:nth-of-type(2) td:nth-of-type(1) a", "href");
-                if (wsQueryUrl == null){ log.warn("WhoScored search result is empty for player: {}", e.getKey()); continue;}
-                if (!(isWSNameContainsPesDbName(ScrapeHelper.getElementDataOwn(wsSearchResult, "div.search-result tr:nth-of-type(2) td:nth-of-type(1) a"), e.getKey()))) {
-                    log.warn("WhoScored player name does not contain PesDb name");
-                    continue;
-                }
+//                Document tmPlayerPage = Jsoup.connect(transfermarktUrl).get();
+//                String playerName = ScrapeHelper.getElementData(tmPlayerPage, "h1[itemprop=name]");
+//                Document wsSearchResult = Jsoup.connect(WS_SEARCH_BASE_URL + playerName.replaceAll(" ", "+")).get();
+//                String wsQueryUrl = ScrapeHelper.getAttributeValue(wsSearchResult, "div.search-result tr:nth-of-type(2) td:nth-of-type(1) a", "href");
+//                if (wsQueryUrl == null){ log.warn("WhoScored search result is empty for player: {}", e.getKey()); continue;}
+//                if (!(isWSNameContainsPesDbName(ScrapeHelper.getElementDataOwn(wsSearchResult, "div.search-result tr:nth-of-type(2) td:nth-of-type(1) a"), e.getKey()))) {
+//                    log.warn("WhoScored player name does not contain PesDb name");
+//                    continue;
+//                }
                 Thread.sleep(15000);
                 Player player = new Player();
                 player.setTransfermarktUrl(transfermarktUrl);
-                player.setWhoScoredUrl(WS_BASE_URL + wsQueryUrl);
+//                player.setWhoScoredUrl(WS_BASE_URL + wsQueryUrl);
                 player.setPesDbUrl(PESDB_BASE_URL + e.getValue());
                 player.setPsmlUrl(PSML_BASE_URL + psmlQueryUrl);
                 AppUserPlayerId appUserPlayerId = new AppUserPlayerId();
@@ -94,7 +95,7 @@ public class ScrapeFreePlayers implements CommandLineRunner {
     private static Map<String, String> scrapeTableByOverall(Document doc){
         Elements players = ScrapeHelper.getElements(doc, "table.players tr:not(:first-child)");
         return players.stream()
-                .filter(e -> Integer.parseInt(ScrapeHelper.getElementData(e, "td:nth-of-type(9)")) >= overallimit)
+                .filter(e -> Integer.parseInt(ScrapeHelper.getElementDataOwn(e, "td:nth-of-type(9)")) >= overallimit)
                 .collect(Collectors.toMap(e -> ScrapeHelper.getElementData(e, "td:nth-of-type(2) a"), e -> ScrapeHelper.getAttributeValue(e, "td:nth-of-type(2) a", "href").replaceAll("\\.", "")));
 
     }
@@ -109,9 +110,12 @@ public class ScrapeFreePlayers implements CommandLineRunner {
         WebDriver driver = null;
         String html = null;
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--headless");
+        //chromeOptions.addArguments("--headless");
         try {
             driver = new ChromeDriver(chromeOptions);
+            driver.get(url);
+            driver.manage().deleteAllCookies();
+            driver.manage().addCookie(new Cookie.Builder("PHPSESSID", "dd30cdbecced0f98b3483e64f89a7d27").path("/").domain("psml.rs").build());
             driver.get(url);
             Thread.sleep(5000);
             driver.switchTo().frame("content");
